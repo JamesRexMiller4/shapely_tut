429d84416c3acde9dd5391091614d552c39197e0
James Miller <james.rex.miller.4@gmail.com>
2025-03-25 19:53:26 -0600

    Refactor ports.py, add airports.csv, add pic of map

--- ports.py
+++ ports.py
@@ -2,15 +2,20 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point
 import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
 
 ports = pd.read_csv("WPI.csv")
-portgeometry = [Point(xy) for xy in zip(ports['Longitude'], ports['Latitude'])]
-portgeodata = gpd.GeoDataFrame(ports, crs="EPSG:4326", geometry=portgeometry)
-portgeodata = portgeodata.to_crs("EPSG:3395")
+airports = pd.read_csv("airports.csv", delimiter=",", names=['id', 'name', 'country', 'iata', 'icao', 'lat', 'long', 'altitude', 'timezone', 'dst', 'tz', 'type', 'source'])
 
-fig, ax = plt.subplots()
-portgeodata.plot(ax=ax, markersize=1, alpha=1, marker='*', color="green", linewidth=0.1)
+port_geometry = [Point(xy) for xy in zip(ports['Longitude'], ports['Latitude'])]
+port_geodata = gpd.GeoDataFrame(ports, crs="EPSG:4326", geometry=port_geometry)
+
+airport_geometry = [Point(xy) for xy in zip(airports['long'], airports['lat'])]
+airport_geodata = gpd.GeoDataFrame(airports, crs="EPSG:4326", geometry=airport_geometry)
+
+fig, ax = plt.subplots(facecolor="black", subplot_kw={'projection': ccrs.Robinson()})
+ax.patch.set_facecolor('black')
+airport_geodata.plot(ax=ax, transform=ccrs.PlateCarree(), markersize=4, alpha=1, color='crimson', edgecolors='none')
+port_geodata.plot(ax=ax, transform=ccrs.PlateCarree(), markersize=3, alpha=1, edgecolors='none', color='lightblue')
+plt.setp([ax.get_xticklines(), ax.get_yticklines()], color='black')
 plt.show()
-
-
-portgeodata.plot()
