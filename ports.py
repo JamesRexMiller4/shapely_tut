b7cb68966a717f96ae8225615bbd1b158493fd6a
James Miller <james.rex.miller.4@gmail.com>
2025-03-25 19:17:32 -0600

    Refactor: change CRS and add additional kwargs for style

--- ports.py
+++ ports.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 ports = pd.read_csv("WPI.csv")
 portgeometry = [Point(xy) for xy in zip(ports['Longitude'], ports['Latitude'])]
 portgeodata = gpd.GeoDataFrame(ports, crs="EPSG:4326", geometry=portgeometry)
+portgeodata = portgeodata.to_crs("EPSG:3395")
 
 fig, ax = plt.subplots()
-portgeodata.plot(ax=ax)
+portgeodata.plot(ax=ax, markersize=1, alpha=1, marker='*', color="green", linewidth=0.1)
 plt.show()
 
 
